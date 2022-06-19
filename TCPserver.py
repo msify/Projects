b602edb557ba78db2ad9d0888a71daf28b574a02
msify <104089103+msify@users.noreply.github.com>
2022-06-19 18:07:17 +0100

    Update TCPserver.py

--- TCPserver.py
+++ TCPserver.py
@@ -7,4 +7,14 @@ port = 444
 
 serversocket.bind((host, port))
 
-serversocket.listen
+serversocket.listen(3)
+
+while True:
+  clientsocket, address = serversocket.accept()
+  
+  print("received connection from" % str(address))
+  
+  
+  message = 'thank you for connecting to the server' + '\r\n'
+  
+  clientsocket.close()
