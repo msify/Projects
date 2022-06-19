a43d77eb197eaaa2b35beacb355a63ebf8cef91d
msify <104089103+msify@users.noreply.github.com>
2022-06-19 18:17:53 +0100

    Update TCPserver.py

--- CodeCamp/TCPserver.py
+++ CodeCamp/TCPserver.py
@@ -1,1 +1,20 @@
+import socket
 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostbyname()
+port = 488
+
+
+serversocket.bind((host, port))
+
+serversocket.listen(3)
+
+while True:
+  clientsocket, address = serversocket.accept()
+  
+  print("received connection from" % str(address))
+  
+  
+  message = 'thank you for connecting to the server' + '\r\n'
+  clientsocket.send(message.encode('ascii')
+  clientsocket.close()
