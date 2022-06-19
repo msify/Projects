6924b6a1871fefbdccbfc1890ffd959fb346a4f3
msify <104089103+msify@users.noreply.github.com>
2022-06-19 18:26:59 +0100

    Update TCPClient.py

--- CodeCamp/TCPClient.py
+++ CodeCamp/TCPClient.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
 
 import socket
-
+// Creates the Socket
 clientsocket = socket.socket(socket.AF_INET, SOCK_STREAM)
+// Gets the host details 
+host = socket.gethostbyname()
+
+port = 488
+
+
+clientsocket.bind(host, port)
+
+//connects to the socket
+clientsocket.connect((host ,port))
+
+//limits the message recived 
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+
+//prints  the decoded message from the tcp server
+print(message.decode('ascii'))
+
